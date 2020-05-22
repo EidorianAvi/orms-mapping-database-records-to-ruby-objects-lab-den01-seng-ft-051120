dfb6dc3ec809be6d68545ad047bad6fd04f477c1
Rene Avila <reneavila1993@gmail.com>
2020-05-22 04:10:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
       WHERE grade < 12;
     SQL
     
-    DB[:count].execute(sql).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
