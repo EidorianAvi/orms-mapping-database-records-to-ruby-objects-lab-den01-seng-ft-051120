bfa08238763e20cf2d852aa58e63a0b258207793
Rene Avila <reneavila1993@gmail.com>
2020-05-22 04:08:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,20 @@ class Student
 
   def self.all
     sql = <<-SQL
-      SELECT * FROM students
+      SELECT *
+      FROM students;
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9;
     SQL
     
     DB[:conn].execute(sql).map do |row|
