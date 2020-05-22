f4b5f4b284a0c1ea6e67eebce392df7b5ca6b456
Rene Avila <reneavila1993@gmail.com>
2020-05-22 04:17:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,6 +72,17 @@ class Student
     end.first
   end
   
+  def self.all_students_in_grade_X x 
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?;
+    SQL
+    
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+  end
+  
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
